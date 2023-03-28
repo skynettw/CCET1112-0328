ede696800f91c88ff70b05f33fb9fa3ce93580f1
skynettw <skynet.tw@gmail.com>
2023-03-28 10:46:23 +0800

    3rd update

--- 0328/Program.cs
+++ 0328/Program.cs
@@ -41,10 +41,32 @@ namespace _0328
             int[] Scores = { 89, 34, 55, 90, 85 };
             Check(Scores);  // 呼叫函式的方法
         }
+        // 以下的函式用來示範把使用者輸入的數字放到陣列中 
+
+        static void Prog3()
+        {
+            const int MAX_NUM = 50;
+            int[] Scores = new int[MAX_NUM]; //宣告一個有MAX_NUM個int的陣列
+            int Count = 0; //這是用來記錄目前已輸入的數字位置（個數）
+            int Score;
+            Console.Write("score=");
+            Score  = Convert.ToInt32(Console.ReadLine());
+            while(Score>=0)
+            {
+                Scores[Count++] = Score;
+                Console.Write("score=");
+                Score = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // 請寫一段程式碼列出剛剛使用者輸入的所有數字
+            
+
+        }
         static void Main(string[] args)
         {
             //Prog1();
-            Prog2();
+            //Prog2();
+            Prog3();
             Console.ReadLine();
         }
     }
