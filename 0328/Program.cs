5f483c7fb9a576ce792e4cef3cbc31167ef9e982
skynettw <skynet.tw@gmail.com>
2023-03-28 09:52:59 +0800

    加入專案檔案。

---
+++ 0328/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _0328
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Scores = {78, 89, 90, 95, 63, 72, 98, 55, 43 };
+            //    Scores[0] : 78, Scores[1] : 89
+            int Total = 0;
+            for(int i=0; i<Scores.Length; i++)
+            {
+                Total += Scores[i];
+            }
+            Console.WriteLine($"Total:{Total}");
+            Console.ReadLine();
+        }
+    }
+}
